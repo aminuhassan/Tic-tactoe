9d092f8fcffde143ddc713de72e036766bf25287
aminuhassan <75027984+aminuhassan@users.noreply.github.com>
2021-02-19 01:27:03 +0200

    Add files via upload

---
+++ Tic tactoe/Tic tactoe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tic_tactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
